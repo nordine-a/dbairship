182b7a54ad27c008ab47e9d713fe6274b2215f62
Abdessamad Nordine <abdessamadnordine.piva@gmail.com>
2023-08-25 22:07:21 +0200

    project initialization

---
+++ src/main/java/com/chdctc/dbairship/DbairshipApplication.java
@@ -0,0 +1,13 @@
+package com.chdctc.dbairship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbairshipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbairshipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chdctc/dbairship/DbairshipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chdctc.dbairship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbairshipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
